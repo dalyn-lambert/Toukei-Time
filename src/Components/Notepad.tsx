f707d559589d30e4f9dfafdc5bf5abd7c7e7fbc7
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-11 13:20:53 -0600

    [chore] removed console log

--- src/Components/Notepad.tsx
+++ src/Components/Notepad.tsx
@@ -8,7 +8,6 @@ const getData = async () => {
 
 const Notepad = async () => {
   const studyLogs = await getData();
-  console.log(studyLogs);
   return (
     <Window English='Recently Studied' Japanese='最近の勉強'>
       <div className='flex flex-col bg-slate-200 p-4 border-2 border-dark-gray'>
