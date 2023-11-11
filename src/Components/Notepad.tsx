47a50da4752de3298a0234ac40d2870126e7843d
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-11 13:31:27 -0600

    [chore] changed node version

--- src/Components/Notepad.tsx
+++ src/Components/Notepad.tsx
@@ -12,9 +12,9 @@ const Notepad = async () => {
     <Window English='Recently Studied' Japanese='最近の勉強'>
       <div className='flex flex-col bg-slate-200 p-4 border-2 border-dark-gray'>
         {studyLogs.map((log) => (
-          <span key={log.id}>- {log.title}</span>
+          <span key={log.id}>• {log.title}</span>
         ))}
-        <span>| </span>
+        <span>• | </span>
       </div>
     </Window>
   );
