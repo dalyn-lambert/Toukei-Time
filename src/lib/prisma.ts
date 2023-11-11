fdc9d12fa6e1548e21c4dbf6b608a976ed6b5ce0
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-11 14:00:00 -0600

    [chore] updated prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,5 @@
+// https://github.com/prisma/prisma/discussions/10037
+
 import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
@@ -5,10 +7,13 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
