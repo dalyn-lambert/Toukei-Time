e05a91d886e9922ab1e0052eae9b31d861a89dda
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-13 13:58:37 -0600

    [chore] added Hover state to Button component

--- src/Components/StyledButton.tsx
+++ src/Components/StyledButton.tsx
@@ -1,7 +1,11 @@
 import { StyledButtonProps } from '@/lib/types';
 
 function StyledButton(props: StyledButtonProps) {
-  return <button className='bg-green p-2 rounded-md text-center border-2 border-dark-gray'>{props.label}</button>;
+  return (
+    <button className='bg-green hover:bg-dark-green p-2 rounded-md text-center border-2 border-dark-gray'>
+      {props.label}
+    </button>
+  );
 }
 
 export default StyledButton;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,14 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      colors: { yellow: '#EFBE37', pink: '#D39384', green: '#9DA25A', gray: '#B7B6C1', 'dark-gray': '#5A5353' },
+      colors: {
+        yellow: '#EFBE37',
+        pink: '#D39384',
+        green: '#9DA25A',
+        'dark-green': '#7d8148',
+        gray: '#B7B6C1',
+        'dark-gray': '#5A5353',
+      },
     },
   },
   safelist: ['bg-yellow', 'bg-pink', 'bg-green', 'bg-gray'],
