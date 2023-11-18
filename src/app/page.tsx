628b12ba97bd34d3362ec9aef55b90ec0392871c
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-18 14:53:17 -0600

    [component] Created LogIn and Registration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Introduction from '@/Components/Introduction';
+import LogIn from '@/Components/LogIn';
+import Registration from '@/Components/Registration';
 
 export default function LandingPage() {
   return (
-    <main>
+    <main className='flex flex-col gap-4'>
       <Introduction />
+      <LogIn />
+      <Registration />
     </main>
   );
 }
