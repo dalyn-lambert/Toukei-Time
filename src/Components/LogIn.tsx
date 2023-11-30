e01c14247d5e4f7160503a34d5ff2330e4ee74b3
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-30 16:34:46 -0600

    [chore] continued auth

--- src/Components/LogIn.tsx
+++ src/Components/LogIn.tsx
@@ -1,20 +1,33 @@
+'use client';
+
+import { authenticate } from '@/lib/actions';
+import { useFormState } from 'react-dom';
 import StyledButton from './StyledButton';
 import Window from './Window';
 
 function LogIn() {
+  const [state, dispatch] = useFormState(authenticate, undefined);
   return (
     <Window English='Log In' Japanese='ログイン'>
       <>
-        <form>
+        <form action={dispatch}>
           <div className='flex flex-col gap-2'>
             <label htmlFor='email'>Email:</label>
-            <input type='text' id='email' />
+            <input id='email' type='email' name='email' placeholder='Enter your email address' required />
             <label htmlFor='password'>Password:</label>
-            <input type='text' id='password' />
+            <input id='password' type='password' name='password' placeholder='Enter password' required minLength={6} />
           </div>
         </form>
         <div className='pt-2'>
           <StyledButton label='Enter' />
+        </div>
+        {/* need to style error message for myself later */}
+        <div className='flex h-8 items-end space-x-1' aria-live='polite' aria-atomic='true'>
+          {state === 'CredentialsSignin' && (
+            <>
+              <p className='text-sm text-red-500'>Invalid credentials</p>
+            </>
+          )}
         </div>
       </>
     </Window>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Introduction from '@/Components/Introduction';
 import LogIn from '@/Components/LogIn';
-import Registration from '@/Components/Registration';
 
 export default function LandingPage() {
   return (
     <main className='flex flex-col gap-4'>
       <Introduction />
       <LogIn />
-      <Registration />
     </main>
   );
 }
