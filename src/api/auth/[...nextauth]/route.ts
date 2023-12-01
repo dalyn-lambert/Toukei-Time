2e9be0080a51429311db27a5362364c5c28c60cc
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-11-30 21:30:07 -0600

    [chore] fixed prisma issue

---
+++ src/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+// https://next-auth.js.org/configuration/initialization#route-handlers-app
+// https://dev.to/nelsonchege13/setting-up-next-auth-with-the-new-app-directory-318b 
+
+// import NextAuth, { type NextAuthOptions } from "next-auth";
+
+
+// export const authOptions: NextAuthOptions = {
+//   providers: []
+//   // authorize
+// }
+
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
