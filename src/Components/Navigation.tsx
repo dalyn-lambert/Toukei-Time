453dd40b9b0ccd82d7b28d268e880b27ae1321d0
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-12-16 18:44:24 -0600

    [chore] reordered nav links

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -4,12 +4,11 @@ function Navigation() {
   return (
     <div className=' bg-gray p-3 drop-shadow-lg border-2 border-dark-gray shadow-inner shadow-white '>
       <div className='flex flex-row text-xs gap-4'>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/view-logs'}>View Logs</Link>
-        <Link href={'/log-studies'}>Log Studies</Link>
         <Link href={'/profile'}>Profile</Link>
-        <Link href={'/view-resources'}>View Resources</Link>
         <Link href={'/add-resource'}>Add Resource</Link>
+        <Link href={'/log-studies'}>Log Studies</Link>
+        <Link href={'/view-logs'}>View Logs</Link>
+        <Link href={'/view-resources'}>View Resources</Link>
       </div>
     </div>
   );
