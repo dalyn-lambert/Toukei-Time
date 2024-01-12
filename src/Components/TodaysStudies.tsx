961eaff87a4efb90518be7c2bdc3b3af36fc3993
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-01-12 17:27:29 -0600

    [chore] re-seeded db to check today studies icons

--- src/Components/TodaysStudies.tsx
+++ src/Components/TodaysStudies.tsx
@@ -19,15 +19,15 @@ const TodaysStudies = async () => {
     <Window English="Today's Studies" Japanese='今日の勉強'>
       <Notepad>
         <span className='border-b-2 border-black'>{todayJapanese}</span>
-        <>
+        <div className='pt-2'>
           {studyLogs.map((log) => (
-            <div key={log.id} className='flex flex-row text-sm pb-4 items-center'>
+            <div key={log.id} className='flex flex-row pb-2 items-center'>
               <span className='pr-2 shrink-0'>{getIconForCategory(log.category)}</span>
               <span>{log.title}</span>
             </div>
           ))}
           <span>• | </span>
-        </>
+        </div>
       </Notepad>
     </Window>
   );
