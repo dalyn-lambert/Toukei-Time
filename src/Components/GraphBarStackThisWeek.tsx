d0d5c1fd7bc15684ce4ffdc1059078702b73fe70
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-01-31 20:11:38 -0600

    [chore] commented out weekly bar stack chart

--- src/Components/GraphBarStackThisWeek.tsx
+++ src/Components/GraphBarStackThisWeek.tsx
@@ -1,19 +1,18 @@
-import { getStudiesBetweenDates } from '@/lib/data';
-import { getFormattedToday } from '@/lib/utils';
-import { format, subDays } from 'date-fns';
-import Window from './Window';
+// import { getStudiesBetweenDates } from '@/lib/data';
+// import { format, subDays } from 'date-fns';
+// import Window from './Window';
 
-const getData = async (start: string, end: string) => {
-  const data = await getStudiesBetweenDates(start, end);
-  return data;
-};
+// const getData = async (start: string, end: string) => {
+//   const data = await getStudiesBetweenDates(start, end);
+//   return data;
+// };
 
-const GraphBarStackThisWeek = async () => {
-  const end = getFormattedToday();
-  const start = subDays(end, 7);
-  const formattedStart = format(start, 'yyyy-MM-dd');
-  const data = await getData(formattedStart, end);
-  return <Window English="This Week's Study Time" Japanese='今週の勉強時間'></Window>;
-};
+// const GraphBarStackThisWeek = async () => {
+//   const end = getFormattedToday();
+//   const start = subDays(end, 7);
+//   const formattedStart = format(start, 'yyyy-MM-dd');
+//   const data = await getData(formattedStart, end);
+//   return <Window English="This Week's Study Time" Japanese='今週の勉強時間'></Window>;
+// };
 
-export default GraphBarStackThisWeek;
+// export default GraphBarStackThisWeek;
