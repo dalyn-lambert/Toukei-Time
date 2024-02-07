eebfca44512941436a46eda7e1cda4e266a265b7
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-02-07 11:35:45 -0600

    [chore] rough desktop styles

--- src/app/(with-nav)/today/page.tsx
+++ src/app/(with-nav)/today/page.tsx
@@ -3,7 +3,7 @@ import TodaysStudies from '@/Components/TodaysStudies';
 
 export default function TodayPage() {
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 '>
       <TodaysStudies />
       <GraphDonutToday />
     </div>
