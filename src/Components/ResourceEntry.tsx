ff335466b30374acdafe0320788286b717faedcf
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-02-10 16:45:48 -0600

    [chore] added icon to resource entry

--- src/Components/ResourceEntry.tsx
+++ src/Components/ResourceEntry.tsx
@@ -1,4 +1,4 @@
-import { getJapaneseNameforCategory, getTotalTimeForResource } from '@/lib/utils';
+import { getIconForCategory, getJapaneseNameforCategory, getTotalTimeForResource } from '@/lib/utils';
 import { Resource } from '@prisma/client';
 import Link from 'next/link';
 import Window from './Window';
@@ -12,9 +12,14 @@ async function ResourceEntry(resource: Resource) {
         Japanese={getJapaneseNameforCategory(resource.category)}
         category={resource.category}
       >
-        <div className='flex flex-col text-center'>
-          <span className='text-lg'>{resource.name}</span>
-          <span className='text-md'>{totalTime}</span>
+        <div className='flex flex-col items-center'>
+          <div className='flex flex-row gap-2 items-center'>
+            <span>{getIconForCategory(resource.category)}</span>
+            <span className='text-md'>{totalTime}</span>
+          </div>
+          <div className='text-center'>
+            <span className='text-lg'>{resource.name}</span>
+          </div>
         </div>
       </Window>
     </Link>
