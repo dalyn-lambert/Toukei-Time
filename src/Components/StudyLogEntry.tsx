68271e0fc63d986f7c9e43067054e760983b48a9
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-02-13 13:41:08 -0600

    [chore] added time to study log entry

--- src/Components/StudyLogEntry.tsx
+++ src/Components/StudyLogEntry.tsx
@@ -1,3 +1,4 @@
+import { formatJapaneseDate, toHoursAndMinutes } from '@/lib/utils';
 import { UTCDate } from '@date-fns/utc';
 import { StudyLog } from '@prisma/client';
 import { format } from 'date-fns';
@@ -6,13 +7,15 @@ import Window from './Window';
 
 function StudyLogEntry(log: StudyLog) {
   const date = format(new UTCDate(log.date), 'yyyy-MM-dd');
+  const JapaneseDate = formatJapaneseDate(date);
 
   return (
     <Link href={`/${log.id}`} key={log.id}>
       <Window English={log.category} Japanese={log.category} category={log.category}>
         <div className='flex flex-row justify-between'>
           <span>{log.title}</span>
-          <span>{date}</span>
+          <span>{toHoursAndMinutes(log.time)}</span>
+          <span>{JapaneseDate}</span>
         </div>
       </Window>
     </Link>
