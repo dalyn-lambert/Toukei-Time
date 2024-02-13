92b98ff207dae10130368bb7dd8a433fcd08cce9
Dalyn Boyd <dalyn.boyd@gmail.com>
2024-02-13 13:25:42 -0600

    [chore] added framing for today donut graph

--- src/Components/GraphDonutToday.tsx
+++ src/Components/GraphDonutToday.tsx
@@ -25,7 +25,7 @@ const GraphDonutToday = async () => {
   const filteredDailyStats: StudyStat[] = dailyStats.filter((stat) => stat.time !== 0);
   return (
     <Window English="Today's Study Time" Japanese='今日の勉強時間'>
-      <div className='flex flex-row gap-4 py-2 justify-around'>
+      <div className='flex flex-row gap-4 py-2 justify-around  p-4 border-2 bg-dark-gray bg-opacity-10 border-dark-gray'>
         <DonutChart width={175} height={175} data={filteredDailyStats} donutThickness={30} />
         <div className='flex flex-col'>
           {filteredDailyStats.map((stat) => (
--- src/Components/Notepad.tsx
+++ src/Components/Notepad.tsx
@@ -1,7 +1,8 @@
 import { NotepadProps } from '@/lib/types';
 
 function Notepad(props: NotepadProps) {
-  return <div className='flex flex-col bg-slate-200 p-4 border-2 border-dark-gray'>{props.children}</div>;
+  return <div className='flex flex-col bg-slate-200 p-4 border-2 border-dark-gray'>
+    {props.children}</div>;
 }
 
 export default Notepad;
