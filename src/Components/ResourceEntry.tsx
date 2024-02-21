53853d3144a2900ced3389f975ba4df06ab19f72
Dalyn Lambert <jdalynlambert@gmail.com>
2024-02-20 19:18:48 -0600

    [chore] tried new styling for browse page

--- src/Components/ResourceEntry.tsx
+++ src/Components/ResourceEntry.tsx
@@ -1,27 +1,16 @@
-import { getIconForCategory, getJapaneseNameforCategory, getTotalTimeForResource } from '@/lib/utils';
+import { getIconForCategory, getTotalTimeForResource } from '@/lib/utils';
 import { Resource } from '@prisma/client';
 import Link from 'next/link';
-import Window from './Window';
 
 async function ResourceEntry(resource: Resource) {
   const totalTime = await getTotalTimeForResource(resource.id);
   return (
     <Link href={`/browse/${resource.id}`} key={resource.id}>
-      <Window
-        English={getJapaneseNameforCategory(resource.category)}
-        Japanese={getJapaneseNameforCategory(resource.category)}
-        category={resource.category}
-      >
-        <div className='flex flex-col items-center'>
-          <div className='flex flex-row gap-2 items-center'>
-            <span>{getIconForCategory(resource.category)}</span>
-            <span className='text-md'>{totalTime}</span>
-          </div>
-          <div className='text-center'>
-            <span className='text-lg'>{resource.name}</span>
-          </div>
-        </div>
-      </Window>
+      <div className='grid grid-cols-6 gap-2'>
+        <div className='col-start-1 pr-2 shrink-0 g'>{getIconForCategory(resource.category)}</div>
+        <div className='col-start-2 col-span-3 truncate text-sm'>{resource.name}</div>
+        <div className='col-start-5 col-span-2 text-sm text-center'>{totalTime}</div>
+      </div>
     </Link>
   );
 }
--- src/app/(with-nav)/browse/page.tsx
+++ src/app/(with-nav)/browse/page.tsx
@@ -1,4 +1,6 @@
+import Notepad from '@/Components/Notepad';
 import ResourceEntry from '@/Components/ResourceEntry';
+import Window from '@/Components/Window';
 import { getAllResources } from '@/lib/data';
 
 const getData = async () => {
@@ -10,10 +12,14 @@ export default async function BrowsePage() {
   const resources = await getData();
 
   return (
-    <div className='flex flex-col gap-4'>
-      {resources.map((resource) => (
-        <ResourceEntry key={resource.id} {...resource} />
-      ))}
-    </div>
+    <Window English='' Japanese=''>
+      <Notepad>
+        <div className='flex flex-col gap-4'>
+          {resources.map((resource) => (
+            <ResourceEntry key={resource.id} {...resource} />
+          ))}
+        </div>
+      </Notepad>
+    </Window>
   );
 }
