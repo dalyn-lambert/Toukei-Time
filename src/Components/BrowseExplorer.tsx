26f8ed55309f92c402eea369539c87079225d166
Dalyn Lambert <jdalynlambert@gmail.com>
2024-03-16 14:37:26 -0500

    [chore] restored browse page without filtering

--- src/Components/BrowseExplorer.tsx
+++ src/Components/BrowseExplorer.tsx
@@ -1,27 +1,30 @@
-'use client';
-
 import Notepad from '@/Components/Notepad';
 import ResourceEntry from '@/Components/ResourceEntry';
 import Window from '@/Components/Window';
-import { getJapaneseNameforCategory } from '@/lib/utils';
-import { Category, Resource } from '@prisma/client';
-import { useState } from 'react';
+import { getAllResources } from '@/lib/data';
+import { Category } from '@prisma/client';
 
-function BrowseExplorer({ resources }: { resources: Resource[] }) {
+const getData = async () => {
+  const data = await getAllResources();
+  return data;
+};
+
+async function BrowseExplorer() {
   const categories: Category[] = Object.values(Category);
-  const [selectedCategory, setSelectedCategory] = useState('Listening');
-  const filterdResources = resources.filter((resource) => resource.category === selectedCategory);
+  // const [selectedCategory, setSelectedCategory] = useState('Listening');
+  const resources = await getData();
+  // const filterdResources = resources.filter((resource) => resource.category === selectedCategory);
 
   return (
     <Window English='' Japanese=''>
-      <div className='flex flex-row justify-around pb-2'>
+      {/* <div className='flex flex-row justify-around pb-2'>
         {categories.map((category) => (
           <div key={category} className='flex flex-row gap-1'>
             <input type='radio' id={category} name='category' value={category} />
             <label htmlFor={category}>{getJapaneseNameforCategory(category)}</label>
           </div>
         ))}
-      </div>
+      </div> */}
       <Notepad>
         <div className='flex flex-col gap-4'>
           {resources.map((resource) => (
--- src/app/(with-nav)/browse/page.tsx
+++ src/app/(with-nav)/browse/page.tsx
@@ -1,8 +1,9 @@
+import BrowseExplorer from '@/Components/BrowseExplorer';
+
 export default function BrowsePage() {
   return (
     <div className='flex flex-col gap-4 desktop:content-center'>
-      Browse Page
-      {/* <BrowseExplorerWrapper /> */}
+      <BrowseExplorer />
     </div>
   );
 }
