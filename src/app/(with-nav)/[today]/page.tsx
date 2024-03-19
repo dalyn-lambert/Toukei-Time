b05705852d599822579f86e3682ee10ac6f0f54a
Dalyn Lambert <jdalynlambert@gmail.com>
2024-03-19 14:13:19 -0500

    [chore] started work on new log window

--- src/app/(with-nav)/[today]/page.tsx
+++ src/app/(with-nav)/[today]/page.tsx
@@ -5,8 +5,8 @@ export default function TodayPage({ params }: { params: { today: string } }) {
   const today = params.today;
   return (
     <div className='flex flex-col gap-4 desktop:content-center'>
+      <GraphDonutToday today={today} />
       <TodaysStudies today={today} />
-      <GraphDonutToday today={today}/>
     </div>
   );
 }
