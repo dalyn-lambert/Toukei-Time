5d06b8dd0a8225f3271210e9cd39ea22f14d4a81
Dalyn Lambert <jdalynlambert@gmail.com>
2024-03-21 16:12:32 -0500

    [feat] able to sort browse page by category and status

--- src/Components/BrowseWindow.tsx
+++ src/Components/BrowseWindow.tsx
@@ -2,7 +2,7 @@
 
 import Window from '@/Components/Window';
 import { getIconForCategory } from '@/lib/utils';
-import { Category, Resource } from '@prisma/client';
+import { Category, Resource, Status } from '@prisma/client';
 import clsx from 'clsx';
 import React from 'react';
 import Notepad from './Notepad';
@@ -10,18 +10,35 @@ import ResourceEntry from './ResourceEntry';
 
 function BrowseWindow({ resources }: { resources: Resource[] }) {
   const categories: Category[] = Object.values(Category);
+  const statuses: Status[] = Object.values(Status);
+
   const [selectedCategory, setSelectedCategory] = React.useState<`${Category}`>(Category.Listening);
-  const filterdResources = resources.filter((resource) => resource.category === selectedCategory);
+  const [selectedStatus, setSelectedStatus] = React.useState<`${Status}`>(Status.Current);
+
+  const filteredResources = resources.filter(
+    (resource) => resource.category === selectedCategory && resource.status === selectedStatus
+  );
 
   return (
-    <Window English='' Japanese=''>
-      <div className='flex flex-row justify-around'>
-        <div className='flex flex-row gap-4'>
+    <Window English='Browse' Japanese=''>
+      <div className='flex flex-col gap-4'>
+        <div className='flex flex-row gap-2 text-sm pl-2'>
+          {statuses.map((status) => (
+            <button
+              key={status}
+              className={clsx('', selectedStatus === status && 'underline')}
+              onClick={() => setSelectedStatus(status)}
+            >
+              {status}
+            </button>
+          ))}
+        </div>
+        <div className='flex flex-row justify-evenly'>
           {categories.map((category) => (
             <button
               key={category}
               className={clsx(
-                ' border-r border-l-2 border-t-2 border-dark-gray p-2 bg-dark-gray bg-opacity-30',
+                'border-r border-l-2 border-t-2 border-dark-gray p-2 bg-dark-gray bg-opacity-30',
                 selectedCategory === category && 'bg-gray shadow-inner shadow-white'
               )}
               onClick={() => setSelectedCategory(category)}
@@ -33,7 +50,7 @@ function BrowseWindow({ resources }: { resources: Resource[] }) {
       </div>
       <Notepad>
         <div className='flex flex-col gap-4'>
-          {filterdResources.map((resource) => (
+          {filteredResources.map((resource) => (
             <ResourceEntry key={resource.id} {...resource} />
           ))}
         </div>
