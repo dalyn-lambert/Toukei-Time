838476aebe09923178ea23d9eda599371821efa4
Dalyn Lambert <jdalynlambert@gmail.com>
2024-03-23 18:43:27 -0500

    [chore] some styling for resource headfer

--- src/Components/ResourceHeader.tsx
+++ src/Components/ResourceHeader.tsx
@@ -1,6 +1,7 @@
 import { ResourceWithTotalTime } from '@/lib/types';
 import { getIconForCategory } from '@/lib/utils';
 import Link from 'next/link';
+import ButtonGeneral from './ButtonGeneral';
 import Window from './Window';
 
 type ResourceHeaderProps = { resource: ResourceWithTotalTime };
@@ -8,12 +9,16 @@ type ResourceHeaderProps = { resource: ResourceWithTotalTime };
 function ResourceHeader({ resource }: ResourceHeaderProps) {
   return (
     <Window English={resource.name} Japanese={resource.name} category={resource.category}>
-      <div className='flex flex-col'>
-        <div className=' pr-2 shrink-0 g'>{getIconForCategory(resource.category)}</div>
-        <div className='  text-sm'>{resource.name}</div>
-        <div className=' text-sm text-center'>{resource.totalTime}</div>
+      <div className='flex flex-col gap-4'>
+        <div className='flex flex-row justify-evenly'>
+          <div className='pr-2 shrink-0'>{getIconForCategory(resource.category)}</div>
+          <div className='text-lg'>{resource.name}</div>
+          <div className='text-lg text-center'>{resource.totalTime}</div>
+        </div>
+        <Link href={`/browse/resource/${resource.id}/update`}>
+          <ButtonGeneral label='Edit Resource' />
+        </Link>
       </div>
-      <Link href={`/browse/resource/${resource.id}/update`}>Edit</Link>
     </Window>
   );
 }
