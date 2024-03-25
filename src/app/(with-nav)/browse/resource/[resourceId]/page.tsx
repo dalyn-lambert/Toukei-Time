867f0c6a4ca440e86b562babf252b8fcb742a537
Dalyn Lambert <jdalynlambert@gmail.com>
2024-03-25 14:49:28 -0500

    [chore] refactored update/delete back to resource page

--- src/app/(with-nav)/browse/resource/[resourceId]/page.tsx
+++ src/app/(with-nav)/browse/resource/[resourceId]/page.tsx
@@ -1,21 +1,15 @@
+import { DeleteResource } from '@/Components/DeleteResource';
 import Notepad from '@/Components/Notepad';
-import ResourceHeader from '@/Components/ResourceHeader';
 import StudyLogEntry from '@/Components/StudyLogEntry';
+import UpdateResource from '@/Components/UpdateResource';
 import Window from '@/Components/Window';
 import { getResourceFromId, getStudyLogsForResource, getTotalTimeForResource } from '@/lib/data';
-import { ResourceWithTotalTime } from '@/lib/types';
 import { StudyLog } from '@prisma/client';
 import { notFound } from 'next/navigation';
 
 const getResource = async (id: number) => {
   const data = await getResourceFromId(id);
   return data;
-};
-
-const addTotalTime = async (resource: ResourceWithTotalTime) => {
-  const totalTime = await getTotalTimeForResource(resource.id);
-  resource.totalTime = totalTime;
-  return resource;
 };
 
 const getStudyLogs = async (id: number) => {
@@ -29,21 +23,23 @@ export default async function ResourcePage({ params }: { params: { resourceId: s
   if (!resource) {
     notFound();
   }
-  const resourceWithTotalTime = await addTotalTime(resource);
+  const totalTime = await getTotalTimeForResource(resource.id);
   const studyLogs: StudyLog[] = await getStudyLogs(id);
 
   return (
     <div className='flex flex-col gap-4'>
-      <ResourceHeader resource={resourceWithTotalTime} />
+      <UpdateResource {...resource} />
       <Window English='All Study Logs for Resource' Japanese='全部のローグ' category={resource.category}>
         <Notepad>
-          <div className='flex flex-col gap-4'>
+          <span className='border-b-2 border-black'>{totalTime}</span>
+          <div className='flex flex-col gap-4 pt-2'>
             {studyLogs.map((studyLog) => (
               <StudyLogEntry key={studyLog.id} {...studyLog} />
             ))}
           </div>
         </Notepad>
       </Window>
+      <DeleteResource id={resource.id} />
     </div>
   );
 }
